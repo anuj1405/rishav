d67549116dae93484c22855cca83cbd261753d44
Anuj Agarwal <a.anuj1405@gmail.com>
2015-12-05 21:38:24 +0530

    First ho

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var server = http.createServer(function(req,res){
 	res.writeHead(200, {'Content-Type':'text/html'});
 	res.end('Hello');
 });
-var port = Number(process.env.PORT || 3000);
-server.listen(port);
+
+server.listen(process.env.PORT || 5000);
