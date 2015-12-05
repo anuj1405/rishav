17cae2c6537bcadb4cc31d390a29e1e080461f12
Anuj Agarwal <a.anuj1405@gmail.com>
2015-12-05 22:46:29 +0530

    se

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ var server = http.createServer(function(req,res){
 	res.writeHead(200, {'Content-Type':'text/html'});
 	res.end('Hello');
 });
-
-server.listen(process.env.PORT || 5000);
+server.listen((process.env.PORT || 5000), function(){
+  console.log('listening on *:5000');
+});
